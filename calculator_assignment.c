789cab515cbe4760330f7a1b1e103f5b7f189aca
Shiva Kumar <shivam518518@gmail.com>
2025-09-30 23:27:10 +0530

    changes done on calculator_assignment

---
+++ calculator_assignment.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_STACK_SIZE 1000
+
+void calculate(const char s[]);
+bool isValidExpression(const char s[]);
+
+int main()
+{
+    char expression[100];
+
+    printf("Enter expression: ");
+    fgets(expression, sizeof(expression), stdin);
+
+    if (!isValidExpression(expression))
+    {
+        printf("Error: Invalid expression.\n");
+        return 0;
+    }
+
+    calculate(expression);
+
+    return 0;
+}
+
+void calculate(const char s[])
+{
+    int stack[MAX_STACK_SIZE];
+    int top = -1;
+    bool isDivisibleByZero = false;
+    int currentNumber = 0;
+    char operation = '+';
+    int len = strlen(s);
+
+    for (int i = 0; i < len; i++)
+    {
+        char currentChar = s[i];
+        if (currentChar == ' ')
+        {
+            continue;
+        }
+        if (isdigit(currentChar))
+        {
+            currentNumber = currentNumber * 10 + (currentChar - '0');
+        }
+
+        if (!isdigit(currentChar) || i == len - 1)
+        {
+            if (operation == '+')
+            {
+                stack[++top] = currentNumber;
+            }
+            else if (operation == '-')
+            {
+                stack[++top] = -currentNumber;
+            }
+            else if (operation == '*')
+            {
+                stack[top] = stack[top] * currentNumber;
+            }
+            else if (operation == '/')
+            {
+                if (currentNumber == 0)
+                {
+                    isDivisibleByZero = true;
+                    break;
+                }
+                stack[top] = stack[top] / currentNumber;
+            }
+            operation = currentChar;
+            currentNumber = 0;
+        }
+    }
+    if (isDivisibleByZero)
+    {
+        printf("Error: Division by zero.");
+    }
+    else
+    {
+        int result = 0;
+        for (int i = 0; i <= top; i++)
+        {
+            result += stack[i];
+        }
+        printf("Result: %d\n", result);
+    }
+}
+
+bool isValidExpression(const char s[])
+{
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+
+        if (s[i] == '\n')
+            continue;
+        if (s[i] == ' ')
+            continue;
+        if (!isdigit(s[i]) && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/' && s[i] != ' ')
+        {
+            return false;
+        }
+    }
+    return true;
+}
