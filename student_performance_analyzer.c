cc732729b3a1feae94b825afadb5c9f9968cdf98
Shiva Kumar <shivam518518@gmail.com>
2025-10-14 12:31:44 +0530

    formating issue and some logical changes
    i have formatted the code as per standards and also validated the input and if same roll number is added then it show error

--- student_performance_analyzer.c
+++ student_performance_analyzer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 #define max_name_length 50
 #define max_input_scan 250
 #define SUBJECT_COUNT 3
@@ -11,82 +12,93 @@ struct Student
     int marksOfThreeSubject[SUBJECT_COUNT]; 	
 };
 
-
-void showStudentPerformance(struct Student studentInstance[],int totalNumberOfStudents);
-
+void showStudentPerformance(struct Student studentInstance[], int totalNumberOfStudents);
 int calculateTotalMarks(int marksOfStudent[], int totalNumberOfSubjects);
- 
 float calculateAverageMarks(int marksOfStudent[], int totalNumberOfSubjects);
-
 char calculateGrade(float averageMarks);
-
 char* gradeToStars(char grade);
-
 void printListOfRollNo(struct Student studentInstance[], int index, int totalStudents);
-
 
 int main()
 {   
     short int totalNumberOfStudents;
        
     printf("Enter Numbers Of Student\n");
-    
-    scanf("%d",&totalNumberOfStudents);
-    
+    scanf("%d", &totalNumberOfStudents);
     getchar();
         
     struct Student studentInstance[totalNumberOfStudents];
 
     for (int i = 0; i < totalNumberOfStudents; i++)
     {
-        char line[max_input_scan];
-        
-        fgets(line, sizeof(line), stdin);
-        
-        line[strcspn(line, "\n")] = '\0';
+        int validInput = 0;
 
-        sscanf(line, "%hd %[^\n0123456789] %d %d %d",
-            &studentInstance[i].rollNo,
-            studentInstance[i].nameOfStudent,
-            &studentInstance[i].marksOfThreeSubject[0],
-            &studentInstance[i].marksOfThreeSubject[1],
-	        &studentInstance[i].marksOfThreeSubject[2]);
-	} 
+        while (!validInput)
+        {
+            char line[max_input_scan];
+            fgets(line, sizeof(line), stdin);
+            line[strcspn(line, "\n")] = '\0';
+
+            int scannedValue = sscanf(line, "%hd %[^\n0123456789] %d %d %d",
+                                      &studentInstance[i].rollNo,
+                                      studentInstance[i].nameOfStudent,
+                                      &studentInstance[i].marksOfThreeSubject[0],
+                                      &studentInstance[i].marksOfThreeSubject[1],
+                                      &studentInstance[i].marksOfThreeSubject[2]);
+
+            if (scannedValue != 5)
+            {
+                printf("Invalid input! Please enter in format: <rollNo> <name> <mark1> <mark2> <mark3>\n");
+                continue;
+            }
+
+            int isDuplicate = 0;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (studentInstance[j].rollNo == studentInstance[i].rollNo)
+                {
+                    isDuplicate = 1;
+                    break;
+                }
+            }
+
+            if (isDuplicate)
+            {
+                printf("Error: Roll number %d already entered!\n", studentInstance[i].rollNo);
+            }
+            else
+            {
+                validInput = 1; 
+            }
+        }
+    }
 	    
-    showStudentPerformance(studentInstance,totalNumberOfStudents);
+    showStudentPerformance(studentInstance, totalNumberOfStudents);
     
     printf("List of Roll Numbers: ");
-    
     printListOfRollNo(studentInstance, 0, totalNumberOfStudents);
-    
     printf("\n");
 
     return 0;
-    
 }
 
-
-void showStudentPerformance( struct Student studentInstance[],int totalNumberOfStudents )
+void showStudentPerformance(struct Student studentInstance[], int totalNumberOfStudents)
 {   
     printf("\n");
     
-	for(int i=0 ; i<totalNumberOfStudents;i++)
+	for (int i = 0; i < totalNumberOfStudents; i++)
 	{
-		int totalMarks = calculateTotalMarks(studentInstance[i].marksOfThreeSubject,SUBJECT_COUNT);
-		
-        float averageMarks = calculateAverageMarks(studentInstance[i].marksOfThreeSubject,SUBJECT_COUNT);
+		int totalMarks = calculateTotalMarks(studentInstance[i].marksOfThreeSubject, SUBJECT_COUNT);
+        float averageMarks = calculateAverageMarks(studentInstance[i].marksOfThreeSubject, SUBJECT_COUNT);
     
-	    printf("Roll: %d\n",studentInstance[i].rollNo);
+	    printf("Roll: %d\n", studentInstance[i].rollNo);    
+	    printf("Name: %s\n", studentInstance[i].nameOfStudent);    
+	    printf("Total: %d\n", totalMarks);    
+	    printf("Average: %.2f\n", averageMarks);   
+	    printf("Grade: %c\n", calculateGrade(averageMarks));
 	    
-	    printf("Name: %s\n",studentInstance[i].nameOfStudent);
-	    
-	    printf("Total: %d\n",totalMarks);
-	    
-	    printf("Average: %.2f\n",averageMarks);
-	    
-	    printf("Grade: %c\n",calculateGrade(averageMarks));
-	    
-	    if(calculateGrade(averageMarks)=='F')
+	    if (calculateGrade(averageMarks) == 'F')
 	    {    
 	        printf("\n");
 	       	continue;
@@ -97,7 +109,6 @@ void showStudentPerformance( struct Student studentInstance[],int totalNumberOfS
 	    }
 	}
 }
-
 
 int  calculateTotalMarks(int marksOfStudent[], int totalNumberOfSubjects)
 {
@@ -111,28 +122,26 @@ int  calculateTotalMarks(int marksOfStudent[], int totalNumberOfSubjects)
     return totalMarks;
 }
 
-
 float calculateAverageMarks(int marksOfStudent[], int totalNumberOfSubjects)
 {
     return (float)calculateTotalMarks(marksOfStudent, totalNumberOfSubjects) / totalNumberOfSubjects;
 }
 
-
 char calculateGrade(float averageMarks)
 {
-	if(averageMarks>85.0 && averageMarks<100.0)
+	if (averageMarks >= 85.0 && averageMarks <= 100.0)
 	{
 		return 'A';
 	}
-    else if(averageMarks>70.0 && averageMarks<85.0)
+    else if (averageMarks >= 70.0 && averageMarks < 85.0)
 	{
 		return 'B';
 	}
-    else if(averageMarks>50.0 && averageMarks<70.0)
+    else if (averageMarks >= 50.0 && averageMarks < 70.0)
 	{
 		return 'C';
 	}
-    else if(averageMarks>35.0 && averageMarks<50.0)
+    else if (averageMarks >= 35.0 && averageMarks < 50.0)
 	{
 		return 'D';
 	}
@@ -142,7 +151,6 @@ char calculateGrade(float averageMarks)
 	}
 }
 
-
 char* gradeToStars(char grade)
 {
     switch (grade)
@@ -151,22 +159,18 @@ char* gradeToStars(char grade)
         {
             return "*****";
         }
-
         case 'B':
         {
             return "****";
         }
-
         case 'C':
         {
             return "***";
         }
-
         case 'D':
         {
             return "**";
         }
-
         default:
         {
             return "";
@@ -174,14 +178,12 @@ char* gradeToStars(char grade)
     }
 }
 
-
 void printListOfRollNo(struct Student studentInstance[], int index, int totalStudents) 
 {
-    if(index >= totalStudents)
+    if (index >= totalStudents)
 	{
 	    return;	
 	}
     printf("%d ", studentInstance[index].rollNo);
-    
     printListOfRollNo(studentInstance, index + 1, totalStudents);
 }
