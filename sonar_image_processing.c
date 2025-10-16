111c452d78f5726f161b968ed04d1d488cf1897b
Shiva Kumar <shivam518518@gmail.com>
2025-10-16 13:07:47 +0530

    sonar_image_processing
    1.it take input as n*n matrix
    2. rotate matrix by 90
    3. then apply smoothing filter to rotated matrix
    4. it also check the dimension is in perticular range or not

---
+++ sonar_image_processing.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printMatrix(int *pointerToArray, int dimensionOfMatrix);
+int validateInput(int noOfDimension);
+void getRotatedMatrix(int *pointerToArray, int dimensionOfMatrix);
+void getSmoothingMatrix(int *pointerToArray, int dimensionOfMatrix);
+void swap(int *firstValue, int *secondValue);
+
+int main(void)
+{
+    srand(time(0));
+    int noOfDimension;
+
+    printf("Please Enter dimension of matrix (2-10): ");
+    scanf("%d", &noOfDimension);
+
+    int matrix[noOfDimension][noOfDimension];
+    int *pointerToArray = (int *)matrix;
+
+    if (validateInput(noOfDimension) == 0)
+    {
+        printf("Please enter a valid dimension between 2 and 10.\n");
+    }
+    else
+    {
+        printf("\nOriginal Matrix:\n");
+
+        for (int i = 0; i < noOfDimension; i++)
+        {
+            for (int j = 0; j < noOfDimension; j++)
+            {
+                *(pointerToArray + i * noOfDimension + j) = rand() % 256;
+            }
+        }
+
+        printMatrix(pointerToArray, noOfDimension);
+
+        printf("\nMatrix After 90° Rotation:\n");
+        getRotatedMatrix(pointerToArray, noOfDimension);
+
+        printf("\nMatrix After Applying 3×3 Smoothing Filter:\n");
+        getSmoothingMatrix(pointerToArray, noOfDimension);
+    }
+
+    return 0;
+}
+
+void printMatrix(int *pointerToArray, int dimensionOfMatrix)
+{
+    for (int i = 0; i < dimensionOfMatrix; i++)
+    {
+        for (int j = 0; j < dimensionOfMatrix; j++)
+        {
+            printf("%d ", *(pointerToArray + i * dimensionOfMatrix + j));
+        }
+        printf("\n");
+    }
+}
+
+int validateInput(int noOfDimension )
+{
+    if(noOfDimension >= 2 && noOfDimension <=10)
+    {
+         return 1;
+    }
+    
+     return 0;
+    
+}
+
+void getRotatedMatrix(int *pointerToArray, int dimensionOfMatrix)
+{
+    for (int i = 0; i < dimensionOfMatrix - 1; i++)
+    {
+        for (int j = i + 1; j < dimensionOfMatrix; j++)
+        {
+            swap(pointerToArray + i * dimensionOfMatrix + j,
+                 pointerToArray + j * dimensionOfMatrix + i);
+        }
+    }
+
+    for (int i = 0; i < dimensionOfMatrix; i++)
+    {
+        for (int j = 0; j < dimensionOfMatrix / 2; j++)
+        {
+            swap(pointerToArray + i * dimensionOfMatrix + j,
+                 pointerToArray + i * dimensionOfMatrix + (dimensionOfMatrix - 1 - j));
+        }
+    }
+
+    printMatrix(pointerToArray, dimensionOfMatrix);
+}
+
+void getSmoothingMatrix(int *pointerToArray, int dimensionOfMatrix)
+{
+    int previousRowSmoothValues[10];
+    int currentRowSmoothValues[10];
+    int canWritePreviousRow = 0;
+
+    for (int rowIndex = 0; rowIndex < dimensionOfMatrix; rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < dimensionOfMatrix; columnIndex++)
+        {
+            int sumOfNeighbours = 0;
+            int validNeighbourCount = 0;
+            int startRowIndex = (rowIndex > 0) ? rowIndex - 1 : rowIndex;
+            int endRowIndex = (rowIndex < dimensionOfMatrix - 1) ? rowIndex + 1 : rowIndex;
+            int startColumnIndex = (columnIndex > 0) ? columnIndex - 1 : columnIndex;
+            int endColumnIndex = (columnIndex < dimensionOfMatrix - 1) ? columnIndex + 1 : columnIndex;
+
+            int *neighbourPointer = pointerToArray + startRowIndex * dimensionOfMatrix + startColumnIndex;
+
+            for (int neighbourRowIndex = startRowIndex; neighbourRowIndex <= endRowIndex; neighbourRowIndex++)
+            {
+                int windowWidth = endColumnIndex - startColumnIndex + 1;
+
+                for (int neighbourColumnOffset = 0; neighbourColumnOffset < windowWidth; neighbourColumnOffset++)
+                {
+                    sumOfNeighbours += *(neighbourPointer + neighbourColumnOffset);
+                    validNeighbourCount++;
+                }
+
+                neighbourPointer += dimensionOfMatrix;
+            }
+
+            *(currentRowSmoothValues + columnIndex) = sumOfNeighbours / validNeighbourCount;
+        }
+
+        if (canWritePreviousRow)
+        {
+            int *matrixWritePointer = pointerToArray + (rowIndex - 1) * dimensionOfMatrix;
+
+            for (int columnOffset = 0; columnOffset < dimensionOfMatrix; columnOffset++)
+            {
+                *(matrixWritePointer + columnOffset) = *(previousRowSmoothValues + columnOffset);
+            }
+        }
+
+        for (int columnOffset = 0; columnOffset < dimensionOfMatrix; columnOffset++)
+        {
+            *(previousRowSmoothValues + columnOffset) = *(currentRowSmoothValues + columnOffset);
+        }
+
+        canWritePreviousRow = 1;
+    }
+
+    if (canWritePreviousRow)
+    {
+        int *matrixWritePointer = pointerToArray + (dimensionOfMatrix - 1) * dimensionOfMatrix;
+        
+        for (int columnOffset = 0; columnOffset < dimensionOfMatrix; columnOffset++)
+        {
+            *(matrixWritePointer + columnOffset) = *(previousRowSmoothValues + columnOffset);
+        }
+    }
+
+    printMatrix(pointerToArray, dimensionOfMatrix);
+}
+
+void swap(int *firstValue, int *secondValue)
+{
+    int temp = *firstValue;
+    *firstValue = *secondValue;
+    *secondValue = temp;
+}
